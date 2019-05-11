bf3a8ff9a6a730fdf32f179dc07bcfc7e5b4930d
lubarinobr <matheus.lubarino1@gmail.com>
2019-05-11 01:01:19 -0300

    add portugal picture

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, StyleSheet, Text, ActivityIndicator } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import api from '../services/api';
-import { Card } from 'react-native-elements';
+import { Card, Image } from 'react-native-elements';
 
 export default class Main extends Component {
 
@@ -22,17 +22,24 @@ export default class Main extends Component {
         this.setState({ visaType: name, status: status});
     }
 
+    getStatus = () => {
+        return `Status: ${this.state.status}`;
+    }
+
     render() {
         const { navigate } = this.props.navigation;
 
         return (
             <View>
                 <Card
-                    image={require('../images/passaporte.png')}>
-                    <Text>Tipo visto: {this.state.visaType}</Text>
-                    <Text style={{marginBottom: 10}}>
-                        Status: {this.state.status}
-                    </Text>
+                    wrapperStyle={{flex: 1}}
+                    imageStyle={{height:200, width:320}}
+                    image={require('../images/bandeira-portugal.jpg')}
+                    featuredTitle="Visto Português"
+                    featuredTitleStyle={style.title}
+                    featuredSubtitleStyle={style.subtitle}
+                    featuredSubtitle={this.getStatus()}
+                    >
                 </Card>
             </View>
         );
@@ -44,3 +51,22 @@ Main.navigationOptions = {
     title: "Home",
     tabBarIcon: <Icon name="home" size={18} color="#999" />
 }
+
+const style = StyleSheet.create({
+    title: {
+        // flex: 1,
+        // justifyContent: 'flex-start',
+        // alignSelf: 'flex-start'
+        alignSelf: 'flex-start',
+        paddingLeft: 10,
+        textShadowOffset: { width: 20, height: 220 },
+        textShadowRadius: 20,
+        textShadowColor: '#000',
+        fontSize:25
+    },
+    subtitle: {
+        alignSelf: 'flex-start',
+        paddingLeft: 10,
+        fontSize:20,
+    }
+})
